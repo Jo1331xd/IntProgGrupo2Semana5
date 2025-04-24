12c4bbd09944dbb6d3a090198d32cc3c5405dd86
jo1331xd <josecarvallo777@gmail.com>
2025-04-24 16:37:46 -0600

    Ejercicio 1 de calificacion de nota

---
+++ actividad5/ejercicio1.py
@@ -0,0 +1,17 @@
+calificacion= int(input("Ingrese su califación estudiantil"))
+
+if calificacion >= 9 and calificacion <= 10:
+    print("Tienes una A")
+
+elif calificacion >= 7 and calificacion <= 8:
+    print("Tienes una B")
+    
+elif calificacion >= 5 and calificacion <= 6:
+    print("Tienes una C")    
+    
+elif calificacion >= 3 and calificacion <= 4:
+    print("Tienes una D")    
+ 
+elif calificacion >= 0 and calificacion <= 2:
+    print("Tienes una F")
+
