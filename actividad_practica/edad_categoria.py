f826739da3a2b9c70bc10a7d7857e6b9a7a24182
jo1331xd <josecarvallo777@gmail.com>
2025-04-27 16:47:44 -0600

    Categoria según la edad

---
+++ actividad_practica/edad_categoria.py
@@ -0,0 +1,8 @@
+edad=int(input("Ingrese su edad: "))
+
+if edad >=10 and edad <=19:
+    print ("Eres un niño")
+elif edad >=20 and edad <=35:
+    print ("Eres un adulto")
+elif edad >= 36 and edad <= 70:
+    print ("Eres un adulto mayor")
