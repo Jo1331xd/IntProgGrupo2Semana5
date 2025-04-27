969adf94e4d7954d89efa0e7e3c1c6a1ab703c75
jo1331xd <josecarvallo777@gmail.com>
2025-04-27 16:56:55 -0600

    ejercicio dia de la semana de forma numerica

--- actividad5/ejercicio3.py
+++ actividad5/ejercicio3.py
@@ -1,2 +1,17 @@
 print(" Presiona cero para salir") 
-dia= int(input("Ingrese el día de la semana de forma númerica(1-7)"))
+dia= int(input("Ingrese el día de la semana de forma númerica(1-7): "))
+
+if dia==1:
+     print("Hoy es lunes")
+elif dia==2:
+       print("Hoy es martes")
+elif dia==3:
+       print("Hoy es miércoles")
+elif dia==4:
+       print("Hoy es jueves")
+elif dia==5:
+       print("Hoy es viernes")
+elif dia==6:
+       print("Hoy es sábado")
+elif dia==7:
+      print("Hoy es domingo")
