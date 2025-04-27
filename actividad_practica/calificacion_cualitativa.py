dc5e5319170f8a9e80f089dd2c1ca905b21e9f25
jo1331xd <josecarvallo777@gmail.com>
2025-04-27 17:07:33 -0600

    Determinar el nivel de estudiante

---
+++ actividad_practica/calificacion_cualitativa.py
@@ -0,0 +1,13 @@
+calificacion=int(input("¿Cual es tu calificación?: "))
+
+if calificacion >=0 and calificacion <=59:
+    print("REPROBADO")
+elif calificacion >=60 and calificacion <=70:
+    print ("Regular")
+elif calificacion >=71 and calificacion <= 75:
+    print ("Bueno")
+elif calificacion >=76 and calificacion <= 90:
+    print ("Muy bueno")
+elif calificacion >=91 and calificacion <= 99:
+    print ("Excelente")
+    
